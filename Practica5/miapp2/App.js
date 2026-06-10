df16d0e3780f833b104094ba3042991fcd2510f1
RafaelBB26 <123047965@upq.edu.mx>
2026-06-09 20:15:47 -0600

    Practica 6

--- Practica5/miapp2/App.js
+++ Practica5/miapp2/App.js
@@ -1,15 +1,32 @@
+//ZONA 1: Importaciones de componentes  y Archivos 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Saludo} from './Components/Saludo';
+import { Saludo2 } from './Components/Saludo2';
 
+//ZONA 2: Main -  Hogar de los componentes 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+            <Text>------------------ Coponente Nativo-----------------</Text>
+
+      <Image source ={ require ('../assets/wave.png')}/>
+
+      <Text>Hola Mundo React Native</Text>
+
+      <Text>------------------ Coponentes Propio Simple-----------------</Text>
+      <Saludo> </Saludo>
+
+      <Text>------------------ Coponentes Propio Compuesto ------------------ </Text>
+      <Saludo2> </Saludo2>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//ZONA 3: Estilos y Posicionamiento 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Practica5/miapp2/Components/Saludo2.js
@@ -0,0 +1,14 @@
+import {Button, Image,Text, View} from 'react-native';
+
+export const Saludo2 = () => {
+    return(
+        <View>
+            
+        <Image source ={ require ('./assets/wave.png')}/>
+        <Text> Soy un componente Compuesto</Text>
+        <Button title="Hola 203"> </Button>
+
+        </View>
+
+    );    
+}
